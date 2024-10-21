290a0d7c8bb1c205ae4cbe9dcfdaf3d9b9cf9b54
Igor M. Santos <111190944+msigor@users.noreply.github.com>
2024-10-20 21:23:00 -0300

    Add files via upload

---
+++ task-service/src/main/java/com/example/taskservice/TaskServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.taskservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class TaskServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TaskServiceApplication.class, args);
+    }
+}
